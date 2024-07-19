c5d93cbea6e0dc6079e0496d6042df70f55a0c36
mariuszo52 <the506miz@gmail.com>
2024-07-19 18:38:28 +0200

    update StartScreen

--- app/src/main/java/com/example/guesstheteam/MainActivity.kt
+++ app/src/main/java/com/example/guesstheteam/MainActivity.kt
@@ -1,47 +1,43 @@
 package com.example.guesstheteam
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.guesstheteam.ui.StartScreen
 import com.example.guesstheteam.ui.theme.GuessTheTeamTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             GuessTheTeamTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                LaunchedEffect(Unit) {
+                    val controller = WindowInsetsControllerCompat(window, window.decorView)
+                    controller.hide(WindowInsetsCompat.Type.navigationBars())
+                    controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+                    StartScreen {}
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GuessTheTeamTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/guesstheteam/ui/StartScreen.kt
+++ app/src/main/java/com/example/guesstheteam/ui/StartScreen.kt
@@ -3,6 +3,7 @@ package com.example.guesstheteam.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,25 +12,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,60 +49,135 @@ fun StartScreen(onPlayClick: () -> Unit) {
         alpha = 0.8f
     )
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
             modifier = Modifier
-                .fillMaxSize()
+                .padding(top = 150.dp)
+                .height(200.dp),
+            fontSize = 60.sp,
+            fontFamily = FontFamily(Font(R.font.bungge_outline)),
+            style = MaterialTheme.typography.bodyLarge.copy(
+                lineHeight = 70.sp,
+                textAlign = TextAlign.Center
+            ),
+            text = "ODGADNIJ DRUŻYNĘ"
+        )
+        Text(
+            modifier = Modifier
+                .padding(top = 50.dp, bottom = 50.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(color = colorResource(id = R.color.darkGreen))
+                .padding(30.dp),
+            fontWeight = FontWeight.Bold,
+            text = "SEZON 2023/24"
+        )
+        Button(
+
+            modifier = Modifier
+                .width(100.dp)
+                .height(100.dp)
+                .border(width = 5.dp, shape = CircleShape, color = Color.White),
+            colors = ButtonColors(
+                containerColor = colorResource(id = R.color.darkGreen),
+                contentColor = Color.White,
+                disabledContentColor = Color.Gray,
+                disabledContainerColor = Color.DarkGray
+            ),
+            onClick = { onPlayClick() }
         ) {
-            Text(
-                fontSize = 60.sp,
-                fontFamily = FontFamily(Font(R.font.bungge_outline)),
-                style = MaterialTheme.typography.bodyLarge.copy(
-                    lineHeight = 70.sp,
-                    textAlign = TextAlign.Center
-                ),
-                text = "ODGADNIJ DRUŻYNĘ"
-            )
-            Text(
+            Image(
                 modifier = Modifier
-                    .padding(top = 50.dp, bottom = 50.dp)
-                    .clip(RoundedCornerShape(20.dp))
-                    .background(Color.DarkGray)
-                    .padding(30.dp),
-                text = "SEZON 2023/24"
-            )
-            Button(
-                modifier = Modifier.width(100.dp),
-                onClick = { onPlayClick() }) {
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_play),
-                    contentDescription = null
+                    .fillMaxSize(),
+                painter = painterResource(id = R.drawable.baseline_play_arrow_24),
+                contentDescription = null,
+
                 )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth()
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(100.dp, Alignment.CenterHorizontally),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .width(100.dp)
             ) {
                 Button(
-                    modifier = Modifier.width(100.dp),
-                    onClick = { onPlayClick() }) {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_settings),
-                        contentDescription = null
-                    )
-                }
-                Button(
-                    modifier = Modifier.width(100.dp),
-                    onClick = { onPlayClick() }) {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_premium),
-                        contentDescription = null
-                    )
-                }
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(100.dp)
+                        .border(width = 5.dp, shape = CircleShape, color = Color.White),
+                    colors = ButtonColors(
+                        containerColor = colorResource(id = R.color.darkGreen),
+                        contentColor = Color.White,
+                        disabledContentColor = Color.Gray,
+                        disabledContainerColor = Color.DarkGray
+                    ),
+                    onClick = { onPlayClick() }
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        painter = painterResource(id = R.drawable.ic_settings),
+                        contentDescription = null,
 
+                        )
+                }
+                Text(
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .align(alignment = Alignment.CenterHorizontally),
+                    text = "Ustawienia",
+                    textAlign = TextAlign.Center
+                )
             }
+
+            Column {
+                Button(
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(100.dp)
+                        .border(
+                            width = 5.dp,
+                            shape = CircleShape,
+                            color = colorResource(id = R.color.gold)
+                        ),
+                    colors = ButtonColors(
+                        containerColor = Color.White,
+                        contentColor = colorResource(id = R.color.gold),
+                        disabledContentColor = Color.Gray,
+                        disabledContainerColor = Color.DarkGray
+                    ),
+                    onClick = { onPlayClick() }
+                ) {
+                    Image(
+                        colorFilter = ColorFilter.tint(colorResource(id = R.color.gold)),
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        painter = painterResource(id = R.drawable.baseline_workspace_premium_24),
+                        contentDescription = null,
+
+                        )
+                }
+                Text(
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .align(alignment = Alignment.CenterHorizontally),
+                    text = "Premium",
+                    textAlign = TextAlign.Center
+                )
+            }
+
         }
     }
+}
 
 
 @Preview(showBackground = true, showSystemUi = true)
