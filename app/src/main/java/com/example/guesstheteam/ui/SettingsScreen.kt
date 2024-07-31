7c5ec4d2fad33eb361f2dcb1ad6d05045de7b75c
mariuszo52 <the506miz@gmail.com>
2024-07-31 12:56:51 +0200

    update SettingsScreen

--- app/src/main/java/com/example/guesstheteam/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/guesstheteam/ui/SettingsScreen.kt
@@ -8,21 +8,28 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -36,25 +43,27 @@ import com.example.guesstheteam.R
 fun SettingsScreen(onBackClick: () -> Unit) {
 
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.White),
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     )
     {
         Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .padding(5.dp)
                 .fillMaxWidth()
-                .height(70.dp),
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+                .height(70.dp)
+
+            ) {
             Button(
-                contentPadding = PaddingValues(10.dp),
                 modifier = Modifier
-                    .size(50.dp),
+                    .width(60.dp),
+                contentPadding = PaddingValues(10.dp),
                 colors = ButtonColors(
                     containerColor = Color.Transparent,
                     contentColor = Color.Black,
@@ -95,6 +104,7 @@ fun SettingsScreen(onBackClick: () -> Unit) {
             horizontalAlignment = Alignment.CenterHorizontally
 
         ) {
+
             Row(
                 modifier = Modifier
                     .padding(5.dp)
@@ -103,38 +113,11 @@ fun SettingsScreen(onBackClick: () -> Unit) {
                 horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(
-                    contentPadding = PaddingValues(5.dp),
-                    shape = RoundedCornerShape(10.dp),
-                    modifier = Modifier
-                        .size(50.dp),
-                    colors = ButtonColors(
-                        containerColor = colorResource(id = R.color.darkGreen),
-                        contentColor = Color.White,
-                        disabledContentColor = Color.Gray,
-                        disabledContainerColor = Color.DarkGray
-                    ),
-
-                    onClick = onBackClick
-                ) {
-                    Image(
-                        colorFilter = ColorFilter.tint(Color.White),
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        painter = painterResource(id = R.drawable.baseline_delete_24),
-                        contentDescription = null
-                    )
-                }
-                Text(
-                    color = Color.Black,
-                    modifier = Modifier
-                        .padding(start = 10.dp),
-                    fontWeight = FontWeight.Bold,
-                    text = stringResource(id = R.string.reset)
-                    
-                )
+                MenuButton(
+                    image = painterResource(id = R.drawable.baseline_delete_24),
+                    text = stringResource(id = R.string.reset),
+                ) { onBackClick() }
             }
-
             Row(
                 modifier = Modifier
                     .padding(5.dp)
@@ -143,37 +126,12 @@ fun SettingsScreen(onBackClick: () -> Unit) {
                 horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(
-                    contentPadding = PaddingValues(5.dp),
-                    shape = RoundedCornerShape(10.dp),
-                    modifier = Modifier
-                        .size(50.dp),
-                    colors = ButtonColors(
-                        containerColor = colorResource(id = R.color.darkGreen),
-                        contentColor = Color.White,
-                        disabledContentColor = Color.Gray,
-                        disabledContainerColor = Color.DarkGray
-                    ),
-
-                    onClick = onBackClick
-                ) {
-                    Image(
-                        colorFilter = ColorFilter.tint(Color.White),
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        painter = painterResource(id = R.drawable.baseline_shield_24),
-                        contentDescription = null
-                    )
-                }
-                Text(
-                    color = Color.Black,
-                    modifier = Modifier
-                        .padding(start = 10.dp),
-                    fontWeight = FontWeight.Bold,
+                MenuButton(
+                    image = painterResource(id = R.drawable.baseline_shield_24),
                     text = stringResource(id = R.string.terms)
                 )
+                { onBackClick() }
             }
-
 
         }
         Column {
@@ -182,6 +140,47 @@ fun SettingsScreen(onBackClick: () -> Unit) {
     }
 }
 
+@Composable
+fun MenuButton(image: Painter, text: String, onClick: () -> Unit) {
+    Button(
+        contentPadding = PaddingValues(0.dp),
+        shape = RectangleShape,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = Color.Transparent
+        ),
+        modifier = Modifier.fillMaxSize(),
+        onClick = onClick
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+
+            ) {
+            Image(
+                colorFilter = ColorFilter.tint(Color.White),
+                modifier = Modifier
+                    .padding(5.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(color = colorResource(id = R.color.darkGreen))
+                    .padding(5.dp)
+                    .fillMaxHeight(),
+                painter = image,
+                contentDescription = null
+            )
+
+            Text(
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(start = 10.dp),
+                fontWeight = FontWeight.Bold,
+                text = text
+            )
+        }
+    }
+}
 
 @Preview
 @Composable
